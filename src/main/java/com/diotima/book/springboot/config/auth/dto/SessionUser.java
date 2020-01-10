930dcae245738a8e00025a7fc1a7f3d4199dfab6
mosara999 <mosraa999@hanmail.net>
2020-01-10 17:46:19 +0900

    5장 내용까지 갱신

---
+++ src/main/java/com/diotima/book/springboot/config/auth/dto/SessionUser.java
@@ -0,0 +1,19 @@
+package com.diotima.book.springboot.config.auth.dto;
+
+import com.diotima.book.springboot.domain.user.User;
+import lombok.Getter;
+
+import java.io.Serializable;
+
+@Getter
+public class SessionUser implements Serializable {
+    private String name;
+    private String email;
+    private String picture;
+
+    public SessionUser(User user) {
+        this.name = user.getName();
+        this.email = user.getEmail();
+        this.picture = user.getPicture();
+    }
+}
--- src/main/java/com/diotima/book/springboot/web/IndexController.java
+++ src/main/java/com/diotima/book/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.diotima.book.springboot.web;
 
+import com.diotima.book.springboot.config.auth.LoginUser;
+import com.diotima.book.springboot.config.auth.dto.SessionUser;
 import com.diotima.book.springboot.service.PostsService;
 import com.diotima.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,16 +10,23 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
 
         model.addAttribute("posts", postsService.findAllDesc());
+
+        if (user != null) {
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
